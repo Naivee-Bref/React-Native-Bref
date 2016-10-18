5c2091b02c5b8403bb2a0118abacae689ff5c79d
irmowan <irmowan@gmail.com>
2016-10-18 21:10:45 +0800

    Reformat code

--- js/page/OriginalScene.js
+++ js/page/OriginalScene.js
@@ -6,93 +6,93 @@
 
 import React, {Component, PropTypes} from 'react';
 import {
-    StyleSheet,
-    Text,
-    View,
-    TouchableHighlight
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight
 } from 'react-native';
 
 
 export default class OriginalScene extends Component {
-    static propTypes = {
-        navigator: PropTypes.object.isRequired
-    };
+  static propTypes = {
+    navigator: PropTypes.object.isRequired
+  };
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <View style={styles.welcome}>
-                    <Text style={styles.welcome_main}>
-                        bref.
-                    </Text>
-                    <TouchableHighlight onPress={() => this.props.navigator.push({scene: 'today'})}>
-                        <Text style={styles.welcome_text}>
-                            Today
-                        </Text>
-                    </TouchableHighlight>
-                    <TouchableHighlight onPress={() => this.props.navigator.push({scene: 'review'})}>
-                        <Text style={styles.welcome_text}>
-                            Review
-                        </Text>
-                    </TouchableHighlight>
-                    <TouchableHighlight>
-                        <Text style={styles.welcome_text}>
-                            Share
-                        </Text>
-                    </TouchableHighlight>
-                    <TouchableHighlight onPress={() => this.props.navigator.push({scene: 'profile'})}>
-                        <Text style={styles.welcome_text}>
-                            Me
-                        </Text>
-                    </TouchableHighlight>
-                </View>
-                <View style={styles.motto}>
-                    <Text style={{color: '#CFCFCF', textAlign: 'left'}}>"</Text>
-                    <Text style={styles.motto_text}>
-                        这是一条非主流个性签名
-                    </Text>
-                    <Text style={{color: '#CFCFCF', textAlign: 'right'}}>"</Text>
-                </View>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.welcome}>
+          <Text style={styles.welcome_main}>
+            bref.
+          </Text>
+          <TouchableHighlight onPress={() => this.props.navigator.push({scene: 'today'})}>
+            <Text style={styles.welcome_text}>
+              Today
+            </Text>
+          </TouchableHighlight>
+          <TouchableHighlight onPress={() => this.props.navigator.push({scene: 'review'})}>
+            <Text style={styles.welcome_text}>
+              Review
+            </Text>
+          </TouchableHighlight>
+          <TouchableHighlight>
+            <Text style={styles.welcome_text}>
+              Share
+            </Text>
+          </TouchableHighlight>
+          <TouchableHighlight onPress={() => this.props.navigator.push({scene: 'profile'})}>
+            <Text style={styles.welcome_text}>
+              Me
+            </Text>
+          </TouchableHighlight>
+        </View>
+        <View style={styles.motto}>
+          <Text style={{color: '#CFCFCF', textAlign: 'left'}}>"</Text>
+          <Text style={styles.motto_text}>
+            这是一条非主流个性签名
+          </Text>
+          <Text style={{color: '#CFCFCF', textAlign: 'right'}}>"</Text>
+        </View>
+      </View>
+    );
+  }
 }
 
 // TODO: Global default color
 // TODO: Not FullScreen
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 10,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#000000'
-    },
-    welcome: {
-        justifyContent: 'center'
-    },
-    welcome_main: {
-        fontSize: 50,
-        color: '#FFFFFF',
-        textAlign: 'center',
-        margin: 10,
-        letterSpacing: 8,
-        fontFamily: 'PingFang SC',
-        justifyContent: 'center'
-    },
-    welcome_text: {
-        textAlign: 'center',
-        color: '#CFCFCF',
-        marginBottom: 5
-    },
-    motto: {
-        marginTop: 100,
-        marginBottom: 10,
-        justifyContent: 'flex-end'
-    },
-    motto_text: {
-        fontSize: 16,
-        fontFamily: 'PingFang SC',
-        color: '#CFCFCF'
-    }
+  container: {
+    flex: 1,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000'
+  },
+  welcome: {
+    justifyContent: 'center'
+  },
+  welcome_main: {
+    fontSize: 50,
+    color: '#FFFFFF',
+    textAlign: 'center',
+    margin: 10,
+    letterSpacing: 8,
+    fontFamily: 'PingFang SC',
+    justifyContent: 'center'
+  },
+  welcome_text: {
+    textAlign: 'center',
+    color: '#CFCFCF',
+    marginBottom: 5
+  },
+  motto: {
+    marginTop: 100,
+    marginBottom: 10,
+    justifyContent: 'flex-end'
+  },
+  motto_text: {
+    fontSize: 16,
+    fontFamily: 'PingFang SC',
+    color: '#CFCFCF'
+  }
 });
--- js/page/ReviewScene.js
+++ js/page/ReviewScene.js
@@ -6,92 +6,92 @@
 
 import React, {Component, PropTypes} from 'react';
 import {
-    StyleSheet,
-    Text,
-    View,
-    ListView,
-    TouchableHighlight
+  StyleSheet,
+  Text,
+  View,
+  ListView,
+  TouchableHighlight
 } from 'react-native';
 
 export default class ReviewScene extends Component {
-    static propTypes = {
-        navigator: PropTypes.object.isRequired
-    };
+  static propTypes = {
+    navigator: PropTypes.object.isRequired
+  };
 
-    constructor() {
-        super();
-        var ds = new ListView.DataSource({rowHasChanged: (r1, r2)=> r1 !== r2});
-        this.state = {
-            // TODO: Add time and location to data source.
-            dataSource: ds.cloneWithRows([
-                '西柚今天翘课被举报了，假装什么都没有发生的样子',
-                '校车上全都是小鲜肉',
-                '食堂的早饭又有油条',
-                '食堂的早饭有油条，激动人心~',
-                '编不下去了摔',
-                '编不下去了',
-                '编不下去了',
-                '编不下去了',
-                '编不下去了',
-                '编不下去了',
-                '编不下去了',
-                '编不下去了',
-                '编不下去了',
-                '编不下去了',
-                '编不下去了',
-                '编不下去了',
-                '编不下去了',
-                '编不下去了',
-                '编不下去了',
-                '编不下去了',
-            ])
-        }
+  constructor() {
+    super();
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2)=> r1 !== r2});
+    this.state = {
+      // TODO: Add time and location to data source.
+      dataSource: ds.cloneWithRows([
+        '西柚今天翘课被举报了，假装什么都没有发生的样子',
+        '校车上全都是小鲜肉',
+        '食堂的早饭又有油条',
+        '食堂的早饭有油条，激动人心~',
+        '编不下去了摔',
+        '编不下去了',
+        '编不下去了',
+        '编不下去了',
+        '编不下去了',
+        '编不下去了',
+        '编不下去了',
+        '编不下去了',
+        '编不下去了',
+        '编不下去了',
+        '编不下去了',
+        '编不下去了',
+        '编不下去了',
+        '编不下去了',
+        '编不下去了',
+        '编不下去了',
+      ])
     }
+  }
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <TouchableHighlight onPress={() => this.props.navigator.pop()}>
-                    <Text style={styles.backButtonText}>Back</Text>
-                </TouchableHighlight>
-                <ListView
-                    dataSource={this.state.dataSource}
-                    renderRow={(rowData) =>
-                        <View style={styles.item}>
-                            <Text style={styles.timelineText}>
-                                {rowData}
-                            </Text>
-                            <Text style={styles.timelineOthers}>
-                                08:45
-                            </Text>
-                        </View>
-                    }
-                />
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableHighlight onPress={() => this.props.navigator.pop()}>
+          <Text style={styles.backButtonText}>Back</Text>
+        </TouchableHighlight>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) =>
+            <View style={styles.item}>
+              <Text style={styles.timelineText}>
+                {rowData}
+              </Text>
+              <Text style={styles.timelineOthers}>
+                08:45
+              </Text>
             </View>
-        );
-    }
+          }
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 10,
-        backgroundColor: '#000000',
-        justifyContent: 'center'
-    },
-    backButtonText: {
-        color: '#FFFFFF',
-        fontSize: 50
-    },
-    item: {
-        marginLeft: 10,
-        padding: 5
-    },
-    timelineText: {
-        color: '#FFFFFF',
-        fontSize: 18
-    },
-    timelineOthers: {
-        color: '#AFAFAF',
-        fontSize: 12
-    }
+  container: {
+    padding: 10,
+    backgroundColor: '#000000',
+    justifyContent: 'center'
+  },
+  backButtonText: {
+    color: '#FFFFFF',
+    fontSize: 50
+  },
+  item: {
+    marginLeft: 10,
+    padding: 5
+  },
+  timelineText: {
+    color: '#FFFFFF',
+    fontSize: 18
+  },
+  timelineOthers: {
+    color: '#AFAFAF',
+    fontSize: 12
+  }
 });
